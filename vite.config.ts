645579ac3f1486147e2050b342b274d989f92a76
buho111 <buho111@gmail.com>
2026-01-17 02:09:38 +0900

    baseurl設定

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		tanstackRouter({
-			target: "react",
-			autoCodeSplitting: true,
-		}),
-		tailwindcss(),
-		react(),
-	],
+  base: "./",
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
 });
