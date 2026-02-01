82e42f1d31c4b10af8e0aa4c578987764178ce4d
buho111 <buho111@gmail.com>
2026-02-01 23:31:12 +0900

    TSエラー修正

--- src/components/pages/ConfigPage.tsx
+++ src/components/pages/ConfigPage.tsx
@@ -90,7 +90,7 @@ export function ConfigPage() {
         </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.map((item, index) => (
+        {data.map((item) => (
           <div
             key={item.id}
             className="bg-white bg-opacity-70 rounded-lg shadow-lg p-4 hover:shadow-xl transition"
